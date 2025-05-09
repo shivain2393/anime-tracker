52e8db362afaf43e27502e232585f35224360698
shivain2393 <shivain.sagar@gmail.com>
2025-05-09 20:35:06 +0530

    made a minor change to About button in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,23 +4,25 @@ import { Button } from "./ui/button";
 import GithubButton from "./GithubButton";
 
 const Navbar = () => {
-  return (
-    <header className="border-b border-zinc-200 dark:border-zinc-800 bg-white/80 dark:bg-zinc-900/80 backdrop-blur-sm sticky top-0 z-50 h-20">
-      <div className="container mx-auto px-4 py-4 flex items-center justify-between h-full">
-        <Link href="/" className="text-2xl md:text-3xl font-bold">
-          AniHubTracker
-        </Link>
-        {/* Search */}
-        <div className="flex items-center md:gap-4">
-          <Button variant='ghost' size='lg'><Link href='/about'>About</Link></Button>
-          <div className="flex items-center gap-1">
-            <ThemeToggler />
-            <GithubButton />
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header className="border-b border-zinc-200 dark:border-zinc-800 bg-white/80 dark:bg-zinc-900/80 backdrop-blur-sm sticky top-0 z-50 h-20">
+            <div className="container mx-auto px-4 py-4 flex items-center justify-between h-full">
+                <Link href="/" className="text-2xl md:text-3xl font-bold">
+                    AniHubTracker
+                </Link>
+                {/* Search */}
+                <div className="flex items-center md:gap-4">
+                    <Button variant="ghost" size="lg" asChild>
+                        <Link href="/about">About</Link>
+                    </Button>
+                    <div className="flex items-center gap-1">
+                        <ThemeToggler />
+                        <GithubButton />
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Navbar;
